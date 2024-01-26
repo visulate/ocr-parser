2405d302cef03bf95c337964cff84017f4271c3b
pgoldtho <peter@goldthorp.com>
2024-01-26 17:22:48 -0500

    Module to unzip and parse TIFF files

---
+++ ocr_module/find_files.py
@@ -0,0 +1,4 @@
+from ocr_module import search_text_files
+#search_text_files("/zfspool/title-test", "warranty AND deed", "warranty-deeds.txt")
+#search_text_files("/zfspool/title-test", "689.02", "warranty-deeds.txt")
+search_text_files("/zfspool/title-deeds", "689.02", "wd-full-list.txt")
